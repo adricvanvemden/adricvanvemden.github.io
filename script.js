11c37cca1a4d9bf9392156ebde659712346c6d83
Adric <adricvanvemden@gmail.com>
2026-01-20 00:21:49 +0100

    update links and descriptions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Select all section elements and navigation links
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
@@ -34,3 +35,76 @@ navLinks.forEach((link) => {
         handleClick(event);
     });
 });
+
+// dynamically generate cards
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((data) => {
+
+        // Experience Section
+        const experienceContainer = document.getElementById('experience-container');
+
+        data.experience.forEach((job) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const highlights = job.highlights.map((highlight) => `<li>${highlight}</li>`).join('');
+
+            const content = `
+        <div>${job.year}</div>
+        <div>
+            <h4>
+                ${job.link ? `
+                <a href="${job.link}" target="_blank"> ${job.title} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                    class="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/>
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
+                </a>` :
+                `${job.title}`}
+            </h4>
+            <p>${job.description}</p>
+            ${highlights ? `<ul>${highlights}</ul>` : ''}
+            <div class="tags">
+                ${job.tags.map((tag) => `<div class="tag">${tag}</div>`).join('')}
+            </div>
+        </div>
+      `;
+
+            card.innerHTML = content;
+            experienceContainer.appendChild(card);
+        });
+
+        // Projects Section
+        const projectsContainer = document.getElementById('projects-container');
+
+        data.projects.forEach((project) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const content = `
+        <div>
+            <h4>
+                ${project.link ? `
+                <a href="${project.link}" target="_blank"> ${project.title} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                    class="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/>
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
+                </a>` :
+                `${project.title}`}
+            </h4>
+            <p> ${project.description} </p>
+            <div class="tags">
+                ${project.tags.map((tag) => `<div class="tag">${tag}</div>`).join('')}
+            </div>
+        </div>
+      `;
+
+
+            card.innerHTML = content;
+            projectsContainer.appendChild(card);
+        });
+
+    })
+    .catch((error) => console.error('Error loading project data:', error));
+
+
